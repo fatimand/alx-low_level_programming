6fdad4fc6f79342a82f57982264b8490ab5776cc
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-13 15:51:40 -0700

    use write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 
 /**
  * main - print String
@@ -7,14 +7,6 @@
  */
 int main(void)
 {
-char str[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-int i = 0;
-while(str[i] != '\0')
-{
-putchar(str[i]);
-i++;
-}
-putchar('\n');
+write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 return (1);
 }
-
