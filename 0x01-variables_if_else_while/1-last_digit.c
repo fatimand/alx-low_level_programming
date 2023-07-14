26bf1bb441969103654feaf1ee4b69fa6bb29498
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-14 06:44:56 -0700

    fix code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ printf("Last digit of %d ", n);
 lastDigit = n % 10;
 if (lastDigit > 5)
 {
-printf("is %d and is greater than 5\n", n);
+printf("is %d and is greater than 5\n", lastDigit);
 }
 else if (lastDigit == 0)
 {
-printf("is %d and is 0\n", n);
+printf("is %d and is 0\n", lastDigit);
 }
 else if (lastDigit < 6)
 {
-printf("is %d and is less than 6 and not 0\n", n);
+printf("is %d and is less than 6 and not 0\n", lastDigit);
 }
 return (0);
 }
