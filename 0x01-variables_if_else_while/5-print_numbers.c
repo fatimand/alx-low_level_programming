3db127a96d6ca0e1a1687fb3fb26672f9ad499f8
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-14 07:54:18 -0700

    prints number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print number
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char a;
+a = 48;
+while (a < 58)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
