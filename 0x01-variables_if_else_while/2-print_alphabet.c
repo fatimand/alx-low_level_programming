01767ee27ac91622bd52380a6b3a733a25ac00d9
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-14 07:33:10 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int a;
+a = 97;
+while (a < 123)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
