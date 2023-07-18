1d223bca3c33c3b4be7e0c318262f21e00bd1992
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-18 05:56:38 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+
+/**
+ * main - control number
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+int len;
+char str[] = "_putchar";
+len = strlen(str);
+for (i=0;i < len ;i++)
+{
+	putchar(str[i]);
+}
+putchar('\n');
+return (0);
+}
