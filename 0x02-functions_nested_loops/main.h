d0940ddcdc28fea018bdf618af926e75ac46c02c
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-18 17:42:34 -0700

    abs

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 void _putchar(char c);
 void print_alphabet(void);
@@ -8,3 +9,4 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
