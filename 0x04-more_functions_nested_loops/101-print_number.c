2a443f4e5b878be606f33986c8d2f6712690a7d7
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-20 19:37:28 -0700

    Fix code

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,15 +7,16 @@
  */
 void print_number(int n)
 {
+	unsigned int nbr = n;
 	if (n < 0)
 	{
-		n = -n;
+		nbr = -n;
 		_putchar('-');
 	}
 
-	if (n / 10 > 0)
+	if ((nbr / 10) > 0)
 	{
-		print_number(n / 10);
+		print_number(nbr / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((nbr % 10) + '0');
 }
