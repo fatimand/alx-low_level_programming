e685c7769820f618946be7f88f3a1b2e940de260
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-20 19:27:19 -0700

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,17 +13,10 @@ void print_number(int n)
 		_putchar('-');
 		print_number(n);
 	}
-	else if (n == 0)
-	{
-		_putchar(n + '0');
-	}
-	else
-	{
 
-		if (n / 10 > 0)
-		{
-			print_number(n / 10);
-		}
-		_putchar((n % 10) + '0');
+	if (n / 10 > 0)
+	{
+		print_number(n / 10);
 	}
+	_putchar((n % 10) + '0');
 }
