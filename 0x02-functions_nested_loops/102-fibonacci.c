b4cbe2773e955c04b13b73d113419a36cfc921de
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-21 13:41:51 -0700

    Fix code

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,11 +3,24 @@
 
 void fibo(int n, long a, long b);
 
+/**
+ * main - check Code
+ * Return: 0
+ */
 int main(void)
 {
 	fibo(50, 0, 1);
 	printf("\n");
+	return (0);
 }
+/**
+ * fibo - fibonacci function
+ * @n: param
+ * @a: param
+ * @b: param
+ * Return: void
+ */
+
 void fibo(int n, long a, long b)
 {
 	if (n == 0)
@@ -20,7 +33,7 @@ void fibo(int n, long a, long b)
 		printf("%lu", a + b);
 		return;
 	}
-	else 
+	else
 	{
 		if (n <= 50)
 		{
@@ -29,7 +42,7 @@ void fibo(int n, long a, long b)
 		else
 		{
 			printf("%lu", a + b);
-		}	
+		}
 		fibo(n - 1, b, a + b);
 	}
 }
