cc786dd538642ebda4fd6b7310f9ec17a96ba16c
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-20 18:27:48 -0700

    print fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print Fizz-Buzz test
+ *
+ * Return: Alawys 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
