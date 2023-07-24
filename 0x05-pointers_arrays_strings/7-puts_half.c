5d7d6c7bef39daf66381983554ee63b034748444
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-24 16:22:14 -0700

    Clean code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,18 +3,18 @@
 /**
  * puts_half - print half of a string
  * @str: string
- * Return: void 
+ * Return: void
  */
 void puts_half(char *str)
 {
 	int i, len, n;
-	
+
 	len = strlen(str);
-	n = ( len % 2 ? len - 1 : len);
+	n = (len % 2 ? len - 1 : len);
 
 	for (i = n / 2 ; i < n ; i++)
 	{
-		putchar(str[ len % 2 ? i + 1 : i]);
+		putchar(str[len % 2 ? i + 1 : i]);
 	}
 	putchar('\n');
 }
