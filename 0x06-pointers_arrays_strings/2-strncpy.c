5692fc1d7be1eca0e44c9a0b78541f7aed0b5af6
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-26 06:03:18 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a strings
+ * @dest: first string
+ * @src: second string
+ * @n: size
+ * Return: result
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, lenDest;
+	char *dest1;
+
+	lenDest = strlen(dest);
+	for (i = 0;i < lenDest ; i++)
+	{
+		*(dest1 + i ) = *(dest + i);
+	}
+	memset(dest,'\0', sizeof(dest));
+	for (i = 0; i < n ; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	if ( *(src + n - 1) != '\n')
+	for (i = n ; i < lenDest ; i++)
+	{
+		*(dest + i) = *(dest1 + i);
+	}
+	return (dest);
+}
