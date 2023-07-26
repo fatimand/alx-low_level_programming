35699255b3dd0fb55ae28772bbdf7593981beb55
fatimand <fatimaezzahrae.najid@gmail.com>
2023-07-26 09:05:50 -0700

    Clean code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 	int i, len, len1, len2, val;
 
 	val = 0;
-	len1 = strlen(s1); 
+	len1 = strlen(s1);
 	len2 = strlen(s2);
 	len = (len1 > len2) ? len2 : len1;
 	for (i = 0; i < len ; i++)
@@ -22,19 +22,18 @@ int _strcmp(char *s1, char *s2)
 			break;
 		}
 		else if (s2[i] > s1[i])
-                {
-                        val = -15;
-                        break;
-                }
+		{
+			val = -15;
+			break;
+		}
 	}
 	if (val == 0 && len1 > len)
 	{
 		val = 15;
 	}
 	else if (val == 0 && len2 > len)
-        {
-                val = -15;
-        }
-
-	return (val);	
+	{
+		val = -15;
+	}
+	return (val);
 }
