bb7020faeb0fed22ac0d11dda085db87363a6155
fatimand <fatimaezzahrae.najid@gmail.com>
2023-08-01 12:05:05 -0700

    Fix code

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,10 +6,9 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j, len;
+	int i, j;
 
-	len = strlen(*a) / 8;
-	for (i = 0; i < len; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
