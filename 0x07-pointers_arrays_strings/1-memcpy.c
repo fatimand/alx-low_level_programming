c3a2844f063f7d759fc98c73e26dbd9375f52292
fatimand <fatimaezzahrae.najid@gmail.com>
2023-08-01 07:47:59 -0700

    Fix Code

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i ,j ;
+	int i, j;
+
 	j = n;
 	for (i = 0; i < j && src[i] != '\0' ; i++)
 	{
-		*(dest + i ) = src[i];
+		*(dest + i) = src[i];
 	}
 	return (dest);
 }
